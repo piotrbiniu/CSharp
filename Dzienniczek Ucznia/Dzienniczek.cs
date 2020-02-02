b029a3edf6204e9e321a7fbfb99eeca77d678edc
pzx98433@studenci.wsb.poznan.pl <pzx98433@studenci.wsb.poznan.pl>
2020-02-02 21:36:05 +0100

    .

---
+++ Dzienniczek Ucznia/Dzienniczek.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dzienniczek_Ucznia
+{
+    class Dzienniczek
+    {
+        // stan (zmienne - pola)
+        List<float> oceny;
+
+        //zachowanie
+
+        /// <summary>
+        /// dodawanie ocen
+        /// </summary>
+        /// <param name="ocena"></param>
+        public void AddRating(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+        /// <summary>
+        /// Obliczanie średniej i sumy ocen
+        /// </summary>
+        /// <returns></returns>
+        public float CalculateAverage()
+        {
+            float suma = 0, srednia = 0;
+
+            foreach (var ocena in oceny)
+            {
+                suma += ocena;
+            }
+
+            srednia = suma / oceny.Count();
+
+            return srednia;
+        }
+        /// <summary>
+        /// Wyznaczanie maksymalnej oceny
+        /// </summary>
+        /// <returns></returns>
+        public float pobierzMaxOcene()
+        {
+            return oceny.Max();
+        }
+        /// <summary>
+        /// Wyznaczanie minimalnej oceny
+        /// </summary>
+        /// <returns></returns>
+        public float PobierzMinOcena()
+        {
+            return oceny.Min();
+        }
+    }
+}
